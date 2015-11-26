6296e1b22db9518c4239e5e87fcd9ae69a2ede91
Maksutova Guzel <guzel.maksutova@yandex.ru>
2015-11-26 22:11:06 +0300

    3.14

---
+++ lab3.14/lab3.14.cpp
@@ -0,0 +1,53 @@
+#include "sdt.h"
+#include "convert.h"
+
+int main()
+{
+    Temperature vvod;
+    vector<Temperature> temp, tempC; //temp хранение значений температуры
+                                     //tempC хранение в одной шкале
+    cout << "Enter temperature with its scale: ";
+    while (cin>>vvod)
+    {
+        try
+        {
+            temp.push_back(convert(vvod,Celsius));
+            temp.push_back(convert(vvod,Kelvin));
+            temp.push_back(convert(vvod,Fahrenheit));
+            tempC.push_back(convert(vvod,Celsius));
+        }
+        catch (const invalid_argument& e)
+        {
+            cerr <<e.what();
+        }
+        catch (const logic_error& er)
+        {
+            cerr <<er.what();
+        }
+        catch (...)
+        {
+            cerr <<"Unknow error.\n";
+        }
+        cout << "Enter temperature with its scale: ";
+    }
+    //вывод на экран и в файл
+    cout <<"   C\t   K\t   F\t\n";
+    ofstream out("temperature.txt", ios_base::trunc); //перед записью файл очищается
+    for (int i=0; i<temp.size(); i++)
+    {
+       printf("%6.2f\t",temp[i]);
+       out <<temp[i]<<"\t\t";
+        if ((i+1)%3==0)
+        {
+            cout <<endl;
+            out <<endl;
+        }
+    }
+    out <<"\nMax temperature=" <<MaxTemp(tempC)<<endl;
+    out <<"Min temperature=" <<MinTemp(tempC)<<endl;
+    out <<"Mean temperature=" <<MeanTemp(tempC)<<endl;
+    out.close();
+    cout <<"\nMax temperature=" <<MaxTemp(tempC)<<endl;
+    cout <<"Min temperature=" <<MinTemp(tempC)<<endl;
+    cout <<"Mean temperature=" <<MeanTemp(tempC)<<endl;
+}
---
+++ lab3.14/structure.cpp
@@ -0,0 +1,120 @@
+#include "convert.h"
+#include "sdt.h"
+Temperature::Temperature()
+{
+    this->value=451;
+    this->scale='F';
+}
+
+Temperature::Temperature(double value, Scale scale)
+{
+    this->value=value;
+    this->scale=scale;
+}
+
+//операторы ввода-вывода для структуры
+istream& operator>>(istream& input, Temperature& temp)
+{
+    input>>temp.value >>temp.scale;
+    return input;
+}
+
+ostream& operator<<(ostream& output, const Temperature& temp)
+{
+    output<<temp.value <<temp.scale;
+    return output;
+}
+//операторы ввода-вывода для перечисления
+istream& operator>>(istream& input, Scale& scale)
+{
+    char s;
+    input>>s;
+    switch(s)
+    {
+        case 'C': scale=Celsius;
+        break;
+        case 'K': scale=Kelvin;
+        break;
+        case 'F': scale=Fahrenheit;
+        break;
+        default: input.setstate(ios_base::failbit); //ошибка
+    }
+    return input;
+}
+ostream& operator<<(ostream& output, const Scale& scale)
+{
+    switch(scale)
+    {
+        case Celsius: output<<'C';
+        break;
+        case Kelvin: output<<'K';
+        break;
+        case Fahrenheit: output<<'F';
+        break;
+    }
+    return output;
+}
+//перегрузка операторов
+bool operator>(const Temperature& left, const Temperature& right)
+{
+    return left.value>right.value;
+}
+
+Temperature operator+(const Temperature& left, const Temperature& right)
+{
+    return  {left.value+right.value,
+             left.scale};
+}
+
+Temperature operator+=(Temperature& left, const Temperature& right)
+{
+    left.value+=right.value;
+    return left;
+}
+
+Temperature operator/(Temperature& left, const int division)
+{
+    return  {left.value/division,
+             left.scale};
+}
+
+
+Temperature operator/=(Temperature& left, const int division)
+{
+    left.value/=division;
+    return left;
+}
+
+//пункт 13
+Temperature MaxTemp(vector<Temperature> temp)
+{
+    Temperature MaxT={-373, Celsius};
+    for (int i=0; i < temp.size(); i++)
+    {
+        if (temp[i].value > MaxT.value)
+            MaxT=temp[i];
+    }
+    return MaxT;
+}
+
+Temperature MinTemp(vector<Temperature> temp)
+{
+    Temperature MinT={10000, Celsius};
+    for (int i=0; i < temp.size(); i++)
+    {
+        if (temp[i].value < MinT.value)
+            MinT=temp[i];
+    }
+    return MinT;
+}
+
+Temperature MeanTemp(vector<Temperature> temp)
+{
+    Temperature MeanT={0, Celsius};
+    for (int i=0; i < temp.size(); i++)
+    {
+        MeanT+=temp[i];
+    }
+    MeanT/=temp.size();
+    return MeanT;
+}
